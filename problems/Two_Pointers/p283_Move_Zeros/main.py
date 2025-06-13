653517cd0452ff28d39b7afed87db5cd201fdad1
zhaoaq <hellozhaoaq@gmail.com>
2025-06-13 10:41:34 +0800

    p283-Move Zeros

---
+++ problems/Two_Pointers/p283_Move_Zeros/main.py
@@ -0,0 +1,19 @@
+#语法糖：python中交换两个元组可以直接用nums[l], nums[r] = nums[r], nums[l]。避免了tmp中间变量的使用
+class Solution:
+    def moveZeros(self, nums):
+        left = 0
+        for right in range(len(nums)):
+            if not nums[right]:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+
+#输入格式：0,1,0,3,12
+nums = list(map(int, input().split(",")))
+
+#输入格式：[0,1,0,3,12]
+nums = list(map(int, input()[1:-1].split(",")))
+#print(nums)
+
+solution = Solution()
+solution.moveZeros(nums)
+print(nums)
