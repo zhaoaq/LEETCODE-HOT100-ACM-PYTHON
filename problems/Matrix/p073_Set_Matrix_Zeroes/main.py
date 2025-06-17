fb975596a6412ff241a06198ffff47f965800347
zhaoaq <hellozhaoaq@gmail.com>
2025-06-18 00:43:28 +0800

    feature：p073 Set Matrix Zeroes

--- problems/Matrix/p073_Set_Matrix_Zeroes/main.py
+++ problems/Matrix/p073_Set_Matrix_Zeroes/main.py
@@ -15,9 +15,6 @@ from typing import List
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         rows = len(matrix)
         cols = len(matrix[0])
 
