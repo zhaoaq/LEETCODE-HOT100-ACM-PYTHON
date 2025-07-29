c2379464a8e6af4bcf894ea79de913bce7441a02
zhaoaq <hellozhaoaq@gmail.com>
2025-07-30 00:23:05 +0800

    feature：p141_Linked_List_Cycle(complete）

--- problems/Linked_List/p141_Linked_List_Cycle/main.py
+++ problems/Linked_List/p141_Linked_List_Cycle/main.py
@@ -1,38 +1,42 @@
 # 套圈算法和相交链表一样，快慢指针和getMiddleNode类似
-from problems.Hashing.p049_Group_Anagrams.main import solution
-
 
 class LinkNode:
     def __init__(self, data):
         self.val = data
         self.next = None
 
-
 class Solution:
     def hasCircle(self,head):
-        if not head or not head.next:
+        if not head:
             return False
         slow = head
         fast = head
-        while slow != fast:
-            if not fast or not fast.next:
-                return False
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
 
-        return True
+def main():
+    list_str = input().strip().split()
+    pos = int(input())
 
-list = input().strip().split()
-pos = int(input())
-nodes = [LinkNode(int(x)) for x in list]
-# 构造环形链表
-if pos != -1 and pos < len(nodes):
-    nodes[-1].next = nodes[pos]
-if nodes
+    nodes = [LinkNode(int(x)) for x in list_str]
 
+    # 连接节点
+    for i in range(len(nodes) - 1):
+        if nodes[i]:
+            nodes[i].next = nodes[i + 1]
+    # 构造环形链表
+    if pos != -1 and pos < len(nodes):
+        nodes[len(nodes) - 1].next = nodes[pos]
 
-solution = Solution()
-print(solution.hasCircle(head))
+    solution = Solution()
+    print(solution.hasCircle(nodes[0]))
+
+if __name__ == "__main__":
+    main()
 
 
 
